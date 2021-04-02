7f85fdd5b1add6e6c78637740a7227a69908e771
lisies93 <solorzanolisseth83@gmail.com>
2021-04-02 15:05:53 -0600

    Stub Spring Boot project, connect JPA project dependency

---
+++ ExpensesTracker/src/test/java/com/skilldistillery/expenses/ExpensesTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.expenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
